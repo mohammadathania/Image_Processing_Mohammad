c8402670bed8832a33490425af806398e6ce00d0
mohammadathania <mohammad.athania@somaiya.edu>
2020-06-03 19:43:35 +0530

    Assignments Completed

--- assignment1.py
+++ assignment1.py
@@ -1,10 +1,14 @@
 import cv2 as cv
 cap = cv.VideoCapture(0)
-user_input = int(input("ENTER THE VALUR OF N ....."))
+counter = 0
+user_input = int(input("ENTER THE VALUE OF N ....."))
 while True :
+    counter = counter + 1
     x , frame = cap.read()
     if cv.waitKey(5) & 0xFF == ord('q') :
         break
     flipped = cv.flip(frame,-1)
-    for i in range(0,user_input) :
-        cv.imshow('Image',frame)
+    if (counter <= user_input) :
+        cv.imshow("Image",frame)
+    else :
+        cv.imshow("Image",flipped)
---
+++ assignment2.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+counter = 0
+cap = cv.VideoCapture(0)
+
+while True :
+    x , frame = cap.read()
+    counter = counter + 1
+    flipped_right = cv.flip(cv.transpose(frame,frame),1)
+    flipped_left = cv.flip(cv.transpose(frame, frame),0)
+    if cv.waitKey(5) & 0xFF == ord('q') :
+        break
+    if counter % 2 == 0 :
+        cv.imshow('Image',flipped_right)
+    else:
+        cv.imshow('Image',flipped_left)
