a3b19e73e7a7b620d00fda9d6af0b63170809440
mohammadathania <mohammad.athania@somaiya.edu>
2020-06-03 19:46:33 +0530

    Updated Transpose

--- assignment2.py
+++ assignment2.py
@@ -5,8 +5,8 @@ cap = cv.VideoCapture(0)
 while True :
     x , frame = cap.read()
     counter = counter + 1
-    flipped_right = cv.flip(cv.transpose(frame,frame),1)
-    flipped_left = cv.flip(cv.transpose(frame, frame),0)
+    flipped_right = cv.flip(cv.transpose(frame),1)
+    flipped_left = cv.flip(cv.transpose(frame),0)
     if cv.waitKey(5) & 0xFF == ord('q') :
         break
     if counter % 2 == 0 :
