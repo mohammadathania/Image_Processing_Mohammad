b6edd9d846b62fc3d018cdc85df1a513b157e1e4
mohammadathania <mohammad.athania@somaiya.edu>
2020-06-05 12:23:37 +0530

    Final Commit

--- mohammad_image_processing/Assignment-1/q1.py
+++ mohammad_image_processing/Assignment-1/q1.py
@@ -1,14 +1,14 @@
 import cv2 as cv
 cap = cv.VideoCapture(0)
 counter = 0
-user_input = int(input("ENTER THE VALUE OF N ....."))
+user_input = int(input("ENTER THE VALUE OF N :"))
 while True :
     counter = counter + 1
     x , frame = cap.read()
-    if cv.waitKey(5) & 0xFF == ord('q') :
+    if cv.waitKey(5000) & 0xFF == ord('q') :
         break
     flipped = cv.flip(frame,-1)
-    if (counter <= user_input) :
-        cv.imshow("Image",frame)
-    else :
+    if counter % user_input == 0 :
         cv.imshow("Image",flipped)
+    else :
+        cv.imshow("Image",frame)
--- mohammad_image_processing/Assignment-1/q2.py
+++ mohammad_image_processing/Assignment-1/q2.py
@@ -7,7 +7,7 @@ while True :
     counter = counter + 1
     flipped_right = cv.flip(cv.transpose(frame),1)
     flipped_left = cv.flip(cv.transpose(frame),0)
-    if cv.waitKey(5) & 0xFF == ord('q') :
+    if cv.waitKey(1000) & 0xFF == ord('q') :
         break
     if counter % 2 == 0 :
         cv.imshow('Image',flipped_right)
