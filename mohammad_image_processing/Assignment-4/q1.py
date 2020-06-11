c36f11b864cc1c35d51be1da919e1e5a828d54f3
mohammadathania <mohammad.athania@somaiya.edu>
2020-06-11 19:40:43 +0530

    Assignment-4 Completed

--- mohammad_image_processing/Assignment-4/q1.py
+++ mohammad_image_processing/Assignment-4/q1.py
@@ -6,12 +6,7 @@ cap = cv2.VideoCapture(0)
 def mouse(event, x, y, flags, param):
 	global points, flag
 	if event == cv2.EVENT_LBUTTONDOWN:
-		print((x,y))
-		print(frame_height)
-		print(frame_width)
-		print(frame.shape)
-		print(frame[x:x+frame_width,y:y+frame_height])
-
+		print(frame[(y,x)])
 
 cv2.namedWindow("img")
 cv2.setMouseCallback("img", mouse)
@@ -19,7 +14,5 @@ cv2.setMouseCallback("img", mouse)
 while True :
 	x,frame = cap.read()
 	cv2.imshow('img',frame)
-	frame_width = frame.shape[1]
-	frame_height = frame.shape[0]
 	if cv2.waitKey(1000) and 0xFF == ord('q') :
 		break
